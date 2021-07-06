c3257525d57b528e7133e7dfe7fca29e568f3ab7
The-Briel-Deal <86860060+The-Briel-Deal@users.noreply.github.com>
2021-07-05 21:39:16 -0400

    Updated Project to save to .JSON file type and to handle exceptions
    *Password manager now uses JSON files to store data.
    *Password manager can now create a new JSON file when one does not already exist.

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 from tkinter import *
 from random import choice, randint
 from tkinter import messagebox
-import pyperclip
+import pyclip
+import json
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save_pass():
     global entry_website
+    val_website = entry_website.get()
     global entry_username
+    val_username = entry_username.get()
     global entry_password
-    val_website = entry_website.get().ljust(20, " ")
-    val_username = entry_username.get().ljust(20, " ")
-    val_password = entry_password.get().ljust(20, " ")
+    val_password = entry_password.get()
+    new_data = {
+        val_website: {
+            "email": val_username,
+            "password": val_password
+        }
+    }
     if len(entry_password.get()) > 0 and len(entry_username.get()) > 0 and len(entry_website.get()) > 0:
         is_ok = messagebox.askokcancel(title=val_website, message=f"These are the details entered: \n"
                                                                   f"Email: {val_username}\n"
                                                                   f"Password: {val_password}\nIs it ok to save?")
         if is_ok:
-            with open(file="password.txt", mode="a+") as file:
-                file.write(f"{val_website}|{val_username}|{val_password}\n")
+            try:
+                with open(file="data.json", mode="r") as file:
+                    data = json.load(file)
+                    data.update(new_data)
+                    print(data)
+            except json.decoder.JSONDecodeError or FileNotFoundError:
+                with open(file="data.json", mode="w") as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                with open(file="data.json", mode="w") as file:
+                    json.dump(data, file, indent=4)
+            finally:
+                entry_website.delete(0, END)
+                entry_password.delete(0, END)
     else:
         messagebox.showinfo("Warning!", "Please don't leave any fields blank!")
 # ---------------------------- GENERATE NEW PASSWORD ------------------------------- #
@@ -32,7 +51,7 @@ def generate_pass():
         sp_chars += choice(["%", "-", "#", "$", "^"])
     entry_password.delete(0, "end")
     entry_password.insert(0, f"Doudles{nums}{sp_chars}")
-    pyperclip.copy(f"Doudles{nums}{sp_chars}")
+    pyclip.copy(f"Doudles{nums}{sp_chars}")
     print("updated")
 
 
